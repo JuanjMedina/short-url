3f17ad381b63884c2646e18e0ecdcd45ac3dc067
Juan Medina <jmedinaguerrero847@gmail.com>
2024-09-07 10:57:02 -0500

    refactor: Update shortUrl.dto.ts to add Swagger API property descriptions and examples

--- src/shorturl/dto/shortUrl.dto.ts
+++ src/shorturl/dto/shortUrl.dto.ts
@@ -1,18 +1,31 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, IsUrl } from 'class-validator';
 export class ShortUrlDto {
   @IsUrl()
+  @ApiProperty({
+    example: 'http://accenture.com',
+  })
   URL: string;
 }
 
 export class ShortUrlPersonalizeDto {
+  @ApiProperty({
+    example: 'http://accenture.com',
+  })
   @IsString()
   URL: string;
 
+  @ApiProperty({
+    example: 'Juan',
+  })
   @IsString()
   name: string;
 }
 
 export class UpdateShortUrlDto {
+  @ApiPropertyOptional({
+    example: 'http://youtube.com',
+  })
   @IsString()
   url: string;
 }
