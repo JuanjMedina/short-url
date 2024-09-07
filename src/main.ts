c7bca48a312fd59095d8f6763777825b37da415a
Juan Medina <jmedinaguerrero847@gmail.com>
2024-09-07 10:56:04 -0500

    refactor: Update main.ts to add bearer authentication
    This commit updates the main.ts file to add bearer authentication to the Swagger documentation. This will enhance the security of the API by requiring authentication for accessing the documentation.
    
    Refactor the code to include the addBearerAuth() method from the SwaggerBuilder class.
    
    Closes #123

--- src/main.ts
+++ src/main.ts
@@ -20,13 +20,14 @@ async function bootstrap() {
       'API for generating, managing, and retrieving short URLs. This service allows users to easily convert long, complex URLs into shortened links, track usage statistics, and manage existing links. Ideal for applications that require efficient and user-friendly URL shortening solutions.',
     )
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
   const configService = app.get(ConfigService);
-  const $PORT = configService.get('PORT') || 3000; // Asegúrate de que haya un valor por defecto
+  const $PORT = configService.get('PORT') || 3000;
 
   await app.listen($PORT);
   console.log(`Listening in ${await app.getUrl()}`);
